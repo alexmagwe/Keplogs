37f55dbb3acd943f6a9196b0c6be0047ac2f1e9f
alexmagwe <alexmagwe@gmail.com>
2022-01-26 18:22:21 +0300

    intial commit

---
+++ components/Toolbar.js
@@ -0,0 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image';
+import {useRouter} from 'next/router';
+import styles from '../styles/Toolbar.module.css'
+import logo from '../images/logo.png'
+function Toolbar() {
+  console.log(logo)
+  const router=useRouter()
+  return <div className={styles.main}>
+  <img onClick={()=>router.push('/')} className={styles.logo} src={logo.src} alt='Keplogs' />
+  <div className={styles.navLinks}>
+  <div onClick={()=>router.push('/')}>Home</div>
+  <div onClick={()=>window.location.href='https://github.com/alexmagwe'}>Github</div>
+  <div onClick={()=>window.location.href='https://twitter.com/alexmagwe'}>Twitter</div>
+  </div>
+  </div>
+}
+
+export default Toolbar;
---
+++ pages/post/[slug].js
@@ -0,0 +1,52 @@
+/* eslint-disable @next/next/no-img-element */
+import imageUrlBuilder from "@sanity/image-url";
+import { useState, useEffect } from "react";
+import styles from "../../styles/post.module.css";
+import BlockContent from "@sanity/block-content-to-react";
+export const Post = ({ title, body, image,projectId,dataset }) => {
+  const [imageUrl, setImageUrl] = useState(null);
+  useEffect(() => {
+    const imgBuilder = imageUrlBuilder({
+      projectId,
+      dataset
+    });
+    setImageUrl(imgBuilder.image(image));
+  }, [image]);
+  return (
+    <div className={styles.main}>
+      <h1 className={styles.mainTitle}>{title}</h1>
+      {imageUrl && (
+        <img className={styles.mainImage} src={imageUrl} alt="loading..." />
+      )}
+      <BlockContent blocks={body} />
+    </div>
+  );
+};
+export const getServerSideProps = async (ctx) => {
+  const postSlug = ctx.query.slug;
+  if (!postSlug) {
+    return {
+      notFound: true, //404 response
+    };
+  }
+  const query = encodeURIComponent(
+    `*[_type=="post" && slug.current=="${postSlug}"]`
+  );
+  const url = `https://xa6w1iw2.api.sanity.io/v1/data/query/production?query=${query}`;
+  const resp = await fetch(url).then((res) => res.json());
+  const post = resp.result[0];
+  if (!post) {
+    return { notFound: true };
+  } else {
+    return {
+      props: {
+        body: post.body,
+        title: post.title,
+        image: post.mainImage,
+        projectId:process.env.PROJECT_ID,
+        dataset: process.env.PROJECT_DATASET,
+      },
+    };
+  }
+};
+export default Post;
